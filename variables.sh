b54b2ed214823e50957d0cb4580c354bb1640e7b
TheToriqul <toriqul.int@gmail.com>
2024-11-19 20:55:54 +0800

    chore: changes path for variable

--- variables.sh
+++ variables.sh
@@ -1,4 +1,4 @@
-CONF_SRC=./example.conf
+CONF_SRC=~/example.conf
 CONF_DST=/etc/nginx/conf.d/default.conf
-LOG_SRC=./example.log
+LOG_SRC=~/example.log
 LOG_DST=/var/log/nginx/custom.host.access.log
